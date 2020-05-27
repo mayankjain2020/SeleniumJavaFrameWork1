564622c0a7b636d8eab1efe35ed733d8c83d40d0
mayankjain2020 <mayankjain2020@gmail.com>
2020-05-27 16:31:40 +0530

    adding a new Project

---
+++ JavaPratice/src/Demo_Constructor.java
@@ -0,0 +1,17 @@
+
+public class Demo_Constructor {
+	
+	String name;
+	
+	   //Constructor
+	Demo_Constructor(){
+	      this.name = "BeginnersBook.com";
+	   }
+	
+	
+	   public static void main(String[] args) {
+		   Demo_Constructor obj = new Demo_Constructor();
+	      System.out.println(obj.name);
+	   }
+
+}
